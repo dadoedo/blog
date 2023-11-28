894ffebfae33039216daf2b76c5416981ccdd7e3
david.stredansky <david.stredansky@csretail.cz>
2023-11-28 18:53:22 +0100

    Encore and Frontend setup v1 (not working yet)

--- app/src/Controller/Admin/DashboardController.php
+++ app/src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Candidate;
 use App\Entity\Company;
+use App\Entity\Profession;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -15,8 +16,8 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
+        // return parent::index();
+        return $this->render('admin/index.html.twig');
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
@@ -42,19 +43,20 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-//        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
-        return [
-            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
-
-            MenuItem::section('Users'),
-            MenuItem::linkToCrud('Candidates', 'fa fa-tags', Candidate::class),
-            MenuItem::linkToCrud('Companies', 'fa fa-tags', Company::class),
-
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Companies', 'fa fa-tags', Company::class);
+        yield MenuItem::linkToCrud('Professions', 'fa fa-tags', Profession::class);
+//        return [
+//            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+//
 //            MenuItem::section('Users'),
-//            MenuItem::linkToCrud('Comments', 'fa fa-comment', Comment::class),
-//            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
-        ];
+//            MenuItem::linkToCrud('Candidates', 'fa fa-tags', Candidate::class),
+//            MenuItem::linkToCrud('Companies', 'fa fa-tags', Company::class),
+//
+////            MenuItem::section('Users'),
+////            MenuItem::linkToCrud('Comments', 'fa fa-comment', Comment::class),
+////            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
+//        ];
 
     }
 }
