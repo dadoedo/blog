a45e003788f4b1adac60c70a9b322ed68ca16538
david.stredansky <david.stredansky@csretail.cz>
2023-12-08 15:48:26 +0100

    Added Crud Controllers

--- app/src/Controller/Admin/DashboardController.php
+++ app/src/Controller/Admin/DashboardController.php
@@ -4,15 +4,20 @@ namespace App\Controller\Admin;
 
 use App\Entity\Candidate;
 use App\Entity\Company;
+use App\Entity\Education;
+use App\Entity\JobPost;
 use App\Entity\Profession;
+use App\Entity\ProfessionCategory;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractDashboardController
 {
+//    #[IsGranted('ROLE_ADMIN')]
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
@@ -38,25 +43,25 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('My New Dashboard!');
+            ->setTitle('Work For All');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
+
+        yield MenuItem::section('Users');
         yield MenuItem::linkToCrud('Companies', 'fa fa-tags', Company::class);
+        yield MenuItem::linkToCrud('Candidates', 'fa fa-tags', Candidate::class);
+
+        yield MenuItem::section('Posts');
+        yield MenuItem::linkToCrud('Job Postings', 'fa fa-tags', JobPost::class);
+
+        yield MenuItem::section('Other');
+        yield MenuItem::linkToCrud('Educations', 'fa fa-tags', Education::class);
         yield MenuItem::linkToCrud('Professions', 'fa fa-tags', Profession::class);
-//        return [
-//            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
-//
-//            MenuItem::section('Users'),
-//            MenuItem::linkToCrud('Candidates', 'fa fa-tags', Candidate::class),
-//            MenuItem::linkToCrud('Companies', 'fa fa-tags', Company::class),
-//
-////            MenuItem::section('Users'),
-////            MenuItem::linkToCrud('Comments', 'fa fa-comment', Comment::class),
-////            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
-//        ];
+        yield MenuItem::linkToCrud('Categories of Professions', 'fa fa-tags', ProfessionCategory::class);
 
     }
 }
